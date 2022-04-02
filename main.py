7ee816b3fde4ecf9ce47a95f14253ec34d74c69f
Yusaku_MAEDA13 <jagaimo13@gmail.com>
2022-04-02 10:56:52 +0900

    作業パス，ドライバパスを整理。

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-def path_change():
-    import os
-    print(os.getcwd())
-    print(__file__)
-    if 'Temp' not in __file__:
-        print('ディレクトリを変更')
-        CURRENTPATH = os.path.dirname(__file__)
-        os.chdir(CURRENTPATH)
-    print(os.getcwd())
-
 class Browser:
-    def __init__(self, driver_path):
+    def __init__(self, driver_path, userdata_path):
         #初期設定
         import os
         from msedge.selenium_tools import Edge, EdgeOptions
         options = EdgeOptions()
         options.use_chromium = True
         #options.add_argument('--headless')  #非表示で起動
-        options.add_argument('--user-data-dir=C:/Users/Yusaku/.edgebuf')
+        options.add_argument(f'--user-data-dir={userdata_path}')
         options.add_argument('--profile-directory=Default')#ユーザーとして起動
         options.add_argument("--remote-debugging-port=9222") 
         options.add_argument('--lang=en')
@@ -31,8 +21,8 @@ class Browser:
         driver.implicitly_wait(10) #要素が見つかるまで待つ時間
         self.driver = driver
 class Zaimu_Kaikei(Browser):
-    def __init__(self, driver_path):
-        super().__init__(driver_path)
+    def __init__(self, driver_path, userdata_path):
+        super().__init__(driver_path, userdata_path)
         self.driver.get('https://zaimu-kaikei.kosen-k.go.jp/llas5/view/login.html')
         from selenium.webdriver.support.select import Select
         Select(self.driver.find_element_by_id('cCdSb')).select_by_visible_text('39/39_香川高等専門学校')
@@ -64,45 +54,6 @@ class Zaimu_Kaikei(Browser):
                 self.driver.switch_to_alert().accept()
             except:
                 print('登録待機中…')
-    def input_orders(self):
-        self.driver.get('https://zaimu-kaikei.kosen-k.go.jp/llas5/view/financialAccounting/supply/purchaseRequestDetailsEntry.html')
-        #配分中以外も表示
-        self.driver.find_element_by_id('hibnCheckYsn-1').click()
-        #執行組織の変更
-        self.driver.find_element_by_id('select2-skkuSskYsn-1-container').click()
-        self.driver.find_element_by_class_name('select2-search__field').send_keys('前田')
-        self.driver.find_element_by_xpath('//li[contains(@title,"前田")]').click()
-        #予算の選択
-        self.driver.find_element_by_xpath('//span[contains(text(),"21K18090若手研究（前田祐作）")]').click()
-        #各項目の入力
-        self.driver.find_element_by_id('select2-cCdNuhnPlc-container').click()
-        self.driver.find_element_by_class_name('select2-search__field').send_keys('共同研究ｽﾍﾟｰｽ（１）')
-        self.driver.find_element_by_xpath('//li[contains(@title,"共同研究ｽﾍﾟｰｽ（１）")]' ).click()
-        self.driver.find_element_by_id('cHnName').send_keys('testitem')
-        self.driver.find_element_by_id('cSu').send_keys('2')
-        self.driver.find_element_by_id('cTnk').send_keys('1100')
-        #新規作成
-        self.driver.find_element_by_id('select2-skkuSskYsn-1-container').click()
-        self.driver.find_element_by_id('S_CreateNew').click()
-        while not "登録しました" in self.driver.page_source:
-            import time
-            time.sleep(1)
-            try:
-                self.driver.switch_to_alert().accept()
-            except:
-                print('登録待機中…')
-        #ここから２件目(新規作成ページへ移動すればこの処理は不要)
-        self.driver.find_element_by_id('systemMessage').click()
-        self.driver.find_element_by_id('S_CreateNew').click()
-
-        while not "登録しました" in self.driver.page_source:
-            import time
-            time.sleep(1)
-            try:
-                self.driver.switch_to_alert().accept()
-            except:
-                print('登録待機中…')
-        self.driver.get('https://zaimu-kaikei.kosen-k.go.jp/llas5/view/financialAccounting/supply/purchaseRequestDetailsEntry.html')
 class Order:
     def __init__(self, item, quantity, unit_price, place, organization, budget):
         self.item           = item
@@ -112,12 +63,16 @@ class Order:
         self.organization   = organization
         self.budget         = budget
 
-path_change()
+#作業ディレクトリを指定
 import os
-driver_path = os.getcwd() + '/msedgedriver.exe'
+os.chdir('C:\Users\Yusaku\独立行政法人 国立高等専門学校機構\前田研究室 - General\研究室資料\物品購入')
+#edgeドライバのパスを指定(使用バージョンに合わせたものをダウンロードhttps://developer.microsoft.com/ja-jp/microsoft-edge/tools/webdriver/)
+driver_path = 'C:/Users/Yusaku/Documents/GitHub/msedgedriver.exe'
+#一時データの保存先を指定（どこでもいい）
+userdata_path = 'C:/Users/Yusaku/.edgebuf'
 
-#会計システムにログイン
-browser = Zaimu_Kaikei(driver_path)
+#会計システムにログイン（ユーザー名，パスワードはedgeに保存しておけば自動入力される）
+browser = Zaimu_Kaikei(driver_path, userdata_path)
 
 import pandas as pd
 df = pd.read_csv('ItemList20220401.csv', header=0, encoding = 'shift_jis')
@@ -132,14 +87,4 @@ for index, row in df.iterrows():
     order = Order(item, quantity, unit_price, place, organization, budget)
     #購入依頼を入力
     browser.input_order(order)
-    
 
-
-'''
-browser = Zaimu_Kaikei(driver_path)
-order = Order(item = 'Qrio Card 2枚 Q-CD1', quantity = '5', unit_price = 2200, place = '共同研究ｽﾍﾟｰｽ（１）', organization = '機械共通経費', budget = '(教育研究)研究実施経費')
-browser.input_order(order)
-print('作業完了')
-import time
-time.sleep(60)
-'''
