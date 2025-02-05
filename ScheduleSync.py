ac24aa3a61a30f1dd1f1e9cf4a48527fdba99079
Yusaku_MAEDA13 <jagaimo13@gmail.com>
2025-02-05 10:07:26 +0900

    予定同期状況が終了後も確認できるように修正

--- ScheduleSync.py
+++ ScheduleSync.py
@@ -10,58 +10,81 @@ when you first use selenium, you have to install driver by pip command.
 pip install chromedriver-binary== (your chrome version: eg. 125.0.6422.78)
 """
 
-"""グーグルカレンダーの取得"""
-user_data_path = "C:/Users/Yusaku/AppData/Local/Google/Chrome/User Data/"
-chrome = GoogleCalender(user_data_path)
-chrome.set_id('jagaimo13')
-chrome.get_calender()
-chrome.close()
+while True:
+    try:
 
-#指定カレンダーの結合
+        """グーグルカレンダーの取得"""
+        user_data_path = "C:/Users/Yusaku/AppData/Local/Google/Chrome/User Data/"
+        chrome = GoogleCalender(user_data_path)
+        chrome.set_id('jagaimo13')
+        chrome.get_calender()
+        chrome.close()
 
-calender_list = [
-    'Lab_maedalab17@gmail.com.ics',
-    "Work_jagaimo13@gmail.com.ics",
-    "Office Hour_c04ca1c5bc84b675f8deec736a75ef5f2a3bd9636dc7ad0280f34bb5f0461dd9@group.calendar.google.com.ics",
-    ]
+        #指定カレンダーの結合
 
-gcal = []
-for calender in calender_list:
-    buf = Calender.GoogleCalender('gcal', calender)
-    if gcal == []:
-        gcal = buf
-    else:
-        gcal = buf.union(gcal)
-    
-    print(calender)
-    print(gcal.view())
+        calender_list = [
+            'Lab_maedalab17@gmail.com.ics',
+            "Work_jagaimo13@gmail.com.ics",
+            "Office Hour_c04ca1c5bc84b675f8deec736a75ef5f2a3bd9636dc7ad0280f34bb5f0461dd9@group.calendar.google.com.ics",
+            ]
 
-"""サイボウズカレンダーに自動入力された予定の取得"""
-user_data_path = "C:/Users/Yusaku/AppData/Local/Google/Chrome/User Data/"
-cyboze = Cyboze(user_data_path)
-cyboze.set_id(name='前田　祐作',uid='5791',department='機械工学科',gid='2100')
-cyboze.get_calender()
+        gcal = []
+        for calender in calender_list:
+            buf = Calender.GoogleCalender('gcal', calender)
+            if gcal == []:
+                gcal = buf
+            else:
+                gcal = buf.union(gcal)
+            
+            print(calender)
+            print(gcal.view())
 
-ccal = Calender.CybozeCalender('ccal','./data/CybozeSchedule.csv')
+        """サイボウズカレンダーに自動入力された予定の取得"""
+        user_data_path = "C:/Users/Yusaku/AppData/Local/Google/Chrome/User Data/"
+        cyboze = Cyboze(user_data_path)
+        cyboze.set_id(name='前田　祐作',uid='5791',department='機械工学科',gid='2100')
+        cyboze.get_calender()
 
-"""予定を期間で絞り込み"""
+        ccal = Calender.CybozeCalender('ccal','./data/CybozeSchedule.csv')
 
-now = datetime.now()
-start = now-timedelta(days = 1)
-end =   now+timedelta(days = 365)
+        """予定を期間で絞り込み"""
 
-gcal_filter = gcal.filtering_by_date(start,end)
-ccal_filter = ccal.filtering_by_date(start,end)
+        now = datetime.now()
+        start = now-timedelta(days = 1)
+        end =   now+timedelta(days = 365)
 
-"""サイボウズにしかない予定の削除"""
-delete = ccal_filter.substract(gcal_filter, 'delete')
-delete.view(id=False)
-cyboze.delete_schedule(delete)
+        gcal_filter = gcal.filtering_by_date(start,end)
+        ccal_filter = ccal.filtering_by_date(start,end)
 
-"""グーグルカレンダーにしかない予定の入力"""
-input = gcal_filter.substract(ccal_filter, 'input')
-input.view()
-cyboze.input_schedule(input)
+        """サイボウズにしかない予定の削除"""
+        delete = ccal_filter.substract(gcal_filter, 'delete')
+        if len(delete.events) > 0:
+            delete.view(id=False)
+            cyboze.delete_schedule(delete)
+        else:
+            print("No delete event")
 
-"""終了"""
-cyboze.close(1)
+        """グーグルカレンダーにしかない予定の入力"""
+        input_events = gcal_filter.substract(ccal_filter, 'input')
+        if len(input_events.events) > 0:
+            input_events.view()
+            cyboze.input_schedule(input)
+        else:
+            print("No input event")
+
+        """終了"""
+        cyboze.close(1)
+
+        break
+
+    except Exception as e:
+        print(e)
+        print("error is occured. meybe you have to install driver by pip command.")
+        print("pip install chromedriver-binary== (your chrome version: eg. 125.0.6422.78)")
+        print("or you have to install chromedriver-binary-auto")
+        print("You can see your chrome version by 'chrome://version/'")
+        print("pip install chromedriver-binary-auto")
+        
+        input("If you want to continue, press any key. If you want to stop, press Ctrl + C")
+
+input("finish sync. press enter to close this window.")
